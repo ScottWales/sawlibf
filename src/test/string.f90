9313aee11ce4fe018fafe4709fc7d25a2f35bc42
Scott Wales <scott.wales@unimelb.edu.au>
2012-12-19 12:53:17 +1100

    More init tests

--- src/test/string.f90
+++ src/test/string.f90
@@ -9,11 +9,23 @@ contains
     end subroutine
     subroutine initInt
         type(string) foo
-
         foo = string(9)
-        
-        call assert(foo%length .eq. 1)
-
+        call assert(foo%length .eq. 9)
+        foo%value = "abcd"
+        call assert(foo%value .eq. "abcd")
+    end subroutine
+    subroutine initChar
+        type(string) foo
+        foo = string("abcd")
+        call assert(foo%length .eq. 4)
+        call assert(foo%value .eq. "abcd")
+    end subroutine
+    subroutine initString
+        type(string) :: foo, bar
+        bar = string("abcd")
+        foo = string(bar)
+        call assert(foo%length .eq. 4)
+        call assert(foo%value(:) .eq. "abcd")
     end subroutine
 
 end module
@@ -23,5 +35,7 @@ program test
 
     call declare()
     call initInt()
+    call initChar()
+    call initString()
 
 end program
