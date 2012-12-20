d00fec635eea216b30364f8e7e12c9ff2c2d45ec
Scott Wales <scott.wales@unimelb.edu.au>
2012-12-20 15:10:36 +1100

    Add length function

--- src/lib/sawlibf/string.f90
+++ src/lib/sawlibf/string.f90
@@ -17,28 +17,45 @@
 !   Returns true if string a is equal to string or character(len=*) b
 !
 ! call string%append(string s)
-!   Append s to the end of this
+!   Append s to the end of the string
+! string%length -> integer
+!   Returns the length of the string
+! 
+! Converting to character(len=*):
+!     type(string) :: foo
+!     character(len=:),allocatable ::bar
+!     integer :: length
+!     length = foo%length
+!     allocate(character(length)::bar)
+!     bar = foo
+!     ! ...
+!     deallocate(bar)
+
 
 module sawlibf_string
 implicit none
+    private
     type string
         character(len=:),allocatable,private :: value
-        integer,private :: length
+        integer,private :: valuelength
     contains
         procedure :: appendString
         procedure :: appendCharacterString
         generic :: append => appendString, &
                              appendCharacterString
+
         procedure :: equalString
         procedure :: equalCharacterString
         generic :: operator(.eq.) => equalString, &
                                      equalCharacterString
+
+        procedure :: length
+
         final :: delete_string 
     end type
 
     ! Constructors
     interface string
-        procedure :: constructFromSize
         procedure :: constructFromString
         procedure :: constructFromCharacterString
     end interface
@@ -47,14 +64,17 @@ implicit none
         procedure :: copyCharacterString
         procedure :: toCharacterString
     end interface
+
+    public :: string
+    public :: assignment(=)
 contains
     ! Construct a string given an initial size
     function constructFromSize(value) result(this)
         integer,intent(in) :: value
         type(string) this
 
-        this%length = value
-        allocate(character(this%length)::this%value)
+        this%valuelength = value
+        allocate(character(this%valuelength)::this%value)
         this%value = char(0)
     end function
 
@@ -63,7 +83,7 @@ contains
         type(string),intent(in) :: value
         type(string) this
 
-        this = constructFromSize(value%length)
+        this = constructFromSize(value%valuelength)
         this%value = value%value
     end function
 
@@ -80,7 +100,7 @@ contains
         implicit none
         type(string),intent(inout) :: this
 
-        this%length = 0
+        this%valuelength = 0
         if (allocated(this%value)) then 
             deallocate(this%value)
         end if
@@ -90,17 +110,17 @@ contains
         implicit none
         class(string),intent(inout) :: this
         class(string),intent(in) :: other
-        character(len=this%length) :: tmp
+        character(len=this%valuelength) :: tmp
 
         tmp = this%value(:)
 
-        this%length = this%length + other%length
+        this%valuelength = this%valuelength + other%valuelength
         if (allocated(this%value)) then 
             deallocate(this%value)
         end if
-        allocate(character(this%length)::this%value)
+        allocate(character(this%valuelength)::this%value)
         this%value(1:len(tmp)) = tmp
-        this%value(len(tmp)+1:this%length) = other%value
+        this%value(len(tmp)+1:this%valuelength) = other%value
     end subroutine
 
     subroutine appendCharacterString(this,other)
@@ -119,8 +139,8 @@ contains
         if (allocated(this%value)) then
             deallocate(this%value)
         end if
-        this%length=other%length
-        allocate(character(this%length)::this%value)
+        this%valuelength=other%valuelength
+        allocate(character(this%valuelength)::this%value)
         this%value = other%value
     end subroutine
     subroutine copyCharacterString(this,other)
@@ -153,5 +173,12 @@ contains
         logical :: equal
 
         equal = this%value .eq. other
+    end function
+
+    elemental function length(this) 
+        implicit none
+        class(string),intent(in) :: this
+        integer :: length
+        length = this%valuelength
     end function
 end module
